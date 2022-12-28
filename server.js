f0b3d9e386b69adb351ba108654b22aacf514219
EwanB112 <78657524+EwanB112@users.noreply.github.com>
2022-12-28 16:06:58 +0000

    empty test

--- server.js
+++ server.js
@@ -1,23 +1,5 @@
 const express = require('express')
 const app = express()
-const mysql = require('mysql');
-const cors = require('cors')
-app.use(cors())
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-
-
-const db = mysql.createConnection({
-    host: "database-1.cstenthch6dd.eu-west-2.rds.amazonaws.com",
-    port: "3306",
-    user: "admin",
-    password: "Northleach",
-    database: "LegacyLocker",
-})
-db.connect(function (err) {
-    if (err) throw err;
-    console.log('connected')
-});
 
 app.get("/", (req, res) => {
     console.log("here")
